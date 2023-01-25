22cd52ed75530d13cb94c4558beac713b4d3c3da
luce <kmluce@gmail.com>
2023-01-25 14:26:43 -0800

    day 13 part 2 solved

--- day13/test_puzzle.py
+++ day13/test_puzzle.py
@@ -4,29 +4,28 @@ from puzzle import Puzzle
 
 class MyTestCase(unittest.TestCase):
     def test_demo_part_a(self):
-        demoa = Puzzle("demo_data.txt", "a")
-        demoa.parse()
-        answer = demoa.solve()
-        self.assertEqual(-1, answer)
+        demo_a = Puzzle("demo_data.txt", "a", 1)
+        demo_a.parse()
+        answer = demo_a.solve()
+        self.assertEqual(13, answer)
 
-#    def test_part_a(self):
-#        parta = Puzzle("test_data.txt", "a")
-#        parta.parse()
-#        answer = parta.solve()
-#        self.assertEqual(-1, answer)
-#
-#    def test_demo_part_b(self):
-#        demob = Puzzle("demo_data.txt", "b")
-#        demob.parse()
-#        answer = demob.solve()
-#        self.assertEqual(-1, answer)
-#
-#
-#    def test_part_b(self):
-#        partb = Puzzle("test_data.txt", "b")
-#        partb.parse()
-#        answer = partb.solve()
-#        self.assertEqual(-1, answer)
+    def test_part_a(self):
+        part_a = Puzzle("test_data.txt", "a", 1)
+        part_a.parse()
+        answer = part_a.solve()
+        self.assertEqual(5684, answer)
+
+    def test_demo_part_b(self):
+        demo_b = Puzzle("demo_data.txt", "b", 1)
+        demo_b.parse()
+        answer = demo_b.solve()
+        self.assertEqual(140, answer)
+
+    def test_part_b(self):
+        part_b = Puzzle("test_data.txt", "b", 1)
+        part_b.parse()
+        answer = part_b.solve()
+        self.assertEqual(22932, answer)
 
 
 if __name__ == '__main__':
