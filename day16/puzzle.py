afaf9f56365cdd5b4f45a1b7c0026222badd2db0
luce <kmluce@gmail.com>
2023-03-22 16:39:20 -0700

    day 16, part 1 complete

--- day16/puzzle.py
+++ day16/puzzle.py
@@ -1,4 +1,5 @@
 import re
+import copy
 from utils.debugging import PrintDebug
 
 
@@ -14,8 +15,8 @@ class TunnelNode:
         # pp = pprint.PrettyPrinter(indent=4)
         # pp.pprint(self)
         self.debug.increase_indent()
-        self.debug.print(3, f"node {self.name} with flow {self.flow} and valve status {self.valve_on}")
-        self.debug.print(3, f"     and tunnels are {self.tunnels}")
+        self.debug.print(2, f"node {self.name} with flow {self.flow} and valve status {self.valve_on}")
+        self.debug.print(2, f"     and tunnels are {self.tunnels}")
         self.debug.decrease_indent()
 
     def my_name(self):
@@ -30,16 +31,16 @@ class TunnelNode:
     def update_route(self, old_dest, new_dest, added_weight):
         tunnel_adds = []
         self.debug.increase_indent()
-        self.debug.print(2, f"entered node {self.name}'s update route")
+        self.debug.print(4, f"entered node {self.name}'s update route")
         self.print()
-        self.debug.print(2, f"updating route to {old_dest} to go to {new_dest}")
+        self.debug.print(4, f"updating route to {old_dest} to go to {new_dest}")
         for x in self.tunnels:
             if x[1] == old_dest:
                 tunnel_adds.append([x[0] + added_weight, new_dest, x[2] + new_dest])
                 # x[1] = new_dest
                 # x[0] = x[0] + added_weight
         self.tunnels.extend(tunnel_adds)
-        self.debug.print(2, "route updated, here's new structure:")
+        self.debug.print(4, "route updated, here's new structure:")
         self.print()
         self.debug.decrease_indent()
 
@@ -54,6 +55,12 @@ class TunnelGraph:
         self.root_obj = None
         self.all_valves = []
         self.debug = debug
+        self.num_nodes = 0
+        self.shortest_paths = []
+        self.name_to_index = {}
+        self.INFINITY = 1000000
+        self.MAX_TIMER = 30
+        self.node_set = set()
 
     def add_node(self, name, flow, tunnels, debug=None):
         self.all_valves.append(TunnelNode(name, flow, tunnels, debug))
@@ -62,11 +69,11 @@ class TunnelGraph:
         # pp = pprint.PrettyPrinter(indent=4)
         # pp.pprint(self.all_valves)
         print("")
-        self.debug.print(2, f"PRINTING entire tunnel map")
-        self.debug.increase_indent
+        self.debug.print(2, f"PRINTING entire tunnel map, # nodes = {len(self.all_valves)}")
+        self.debug.increase_indent()
         for i in self.all_valves:
             i.print()
-        self.debug.decrease_indent
+        self.debug.decrease_indent()
 
     def find_node_from_name(self, node_name):
         matches = [x for x in self.all_valves if x.my_name() == node_name]
@@ -77,7 +84,7 @@ class TunnelGraph:
             return matches[0]
 
     def update_route(self, node_name, orig_dest, new_dest, added_weight):
-        self.debug.print(2, f"updating {node_name} from {orig_dest} to {new_dest} and adding weight {added_weight}")
+        self.debug.print(3, f"updating {node_name} from {orig_dest} to {new_dest} and adding weight {added_weight}")
         node_to_update = self.find_node_from_name(node_name)
         node_to_update.update_route(orig_dest, new_dest, added_weight)
         # node_to_update.remove_route(orig_dest)
@@ -90,36 +97,30 @@ class TunnelGraph:
         self.all_valves = [x for x in self.all_valves if x != node_to_remove]
 
     def update_node_paths(self, remove_node):
-        # TODO: this probably does the wrong thing.  It needs to:
-        #       * get all destinations from remove_node.
-        #       * one by one, go to those node destinations and instantiate a path for EACH
-        #         destination in remove_node except the self-referencing one.
-        #       * remove the path directly to remove_node from each of those destination nodes
-        #       * remove the remove_node
         self.debug.increase_indent()
-        self.debug.print(2, f"UPDATING paths to remove node {remove_node.my_name()}:")
+        self.debug.print(3, f"UPDATING paths to remove node {remove_node.my_name()}:")
         remove_node.print()
         for node1_weight, node1_dest, node1_orig_dest in remove_node.my_tunnels():
-            self.debug.print(2, f"tunnel to {node1_dest} originally to {node1_orig_dest} at weight {node1_weight}")
+            self.debug.print(3, f"tunnel to {node1_dest} originally to {node1_orig_dest} at weight {node1_weight}")
             self.debug.increase_indent()
             for node2_weight, node2_dest, node2_orig_dest in remove_node.my_tunnels():
                 if node1_dest != node2_dest:
-                    self.debug.print(2, f"need to update node {node1_dest} to go to {node2_dest} with orig dest {remove_node.my_name()} weight {node2_weight}")
+                    self.debug.print(3, f"need to update node {node1_dest} to go to {node2_dest} with orig dest"
+                                        f"{remove_node.my_name()} weight {node2_weight}")
                     self.update_route(node1_dest, remove_node.my_name(), node2_dest, node2_weight)
             self.debug.decrease_indent()
-            self.debug.print(2, f"removing route to {remove_node.my_name()} from {node1_dest}")
+            self.debug.print(3, f"removing route to {remove_node.my_name()} from {node1_dest}")
             self.remove_route(node1_dest, remove_node.my_name())
         self.debug.decrease_indent()
 
     def optimize_graph(self):
-        empty_nodes = []
         self.debug.print(3, f"Entered Optimize Graph function")
         self.debug.increase_indent()
         self.debug.print(2, f"number of valves is {len(self.all_valves)}")
         empty_nodes = [x for x in self.all_valves if x.my_flow() == 0 and x.my_name() != self.root_name]
         self.debug.print(2, f"number of empty_nodes (aside from root) is {len(empty_nodes)}")
         for i in empty_nodes:
-            self.debug.print(2, f"routing around empty node {i.my_name()}")
+            self.debug.print(3, f"routing around empty node {i.my_name()}")
             self.debug.increase_indent()
             self.update_node_paths(i)
             self.remove_node(i)
@@ -129,15 +130,104 @@ class TunnelGraph:
         self.update_node_paths(self.find_node_from_name(self.root_name))
         self.print()
         self.debug.decrease_indent()
-        print("END")
+        self.debug.print(1, "Done with parsing")
 
-    # def find_path(self):
-    #     current_node = self.find_node_from_name(self.root_name)
-    #     visited_nodes = set()
-    #     flow_val = 0
-    #     timer = 1
-    #     while len(visited_nodes) <= len(self.all_valves) and timer < 30:
-    #         pass
+    def print_path_matrix(self):
+        self.debug.print(2, "")
+        self.debug.print(2, f"PRINTING shortest path matrix")
+        print_string = "  "
+        for i in self.all_valves:
+            print_string = print_string + f"{i.my_name():>8}"
+        self.debug.print(2, print_string)
+        for i in range(self.num_nodes):
+            print_string = ""
+            print_string = print_string + f"{self.all_valves[i].my_name():4}"
+            for j in range(self.num_nodes):
+                print_string = print_string + f"{self.shortest_paths[i][j]:>8}"
+            self.debug.print(2, print_string)
+
+    def find_all_shortest_paths(self):
+        self.num_nodes = len(self.all_valves)
+        self.shortest_paths = [[self.INFINITY for _j in range(self.num_nodes)] for _i in range(self.num_nodes)]
+        for index in range(len(self.all_valves)):
+            self.name_to_index[self.all_valves[index].my_name()] = index
+        self.print_path_matrix()
+        for node in self.all_valves:
+            for weight, dest, orig_dest in node.my_tunnels():
+                self.shortest_paths[self.name_to_index[node.my_name()]][self.name_to_index[dest]] = weight
+        self.print_path_matrix()
+        for k in range(len(self.all_valves)):
+            for i in range(len(self.all_valves)):
+                for j in range(len(self.all_valves)):
+                    if self.shortest_paths[i][j] > self.shortest_paths[i][k] + self.shortest_paths[k][j]:
+                        self.shortest_paths[i][j] = self.shortest_paths[i][k] + self.shortest_paths[k][j]
+        self.node_set = set(self.all_valves)
+        self.print_path_matrix()
+
+    def find_path(self):
+        current_node = self.find_node_from_name(self.root_name)
+        visited_nodes = set()
+        curr_path = []
+        flow_val = 0
+        timer = 0
+        print("")
+        result, new_path = self.find_best_path(current_node, timer, 1, flow_val, visited_nodes, curr_path)
+        self.debug.print(2, f"found result with path:")
+        self.print_path(new_path, 2)
+        return result
+
+    def print_path(self, path, debug_level):
+        self.debug.increase_indent()
+        for node, time in path:
+            self.debug.print(debug_level, f"{time}: {node}")
+        self.debug.decrease_indent()
+
+    def find_best_path(self, curr_node, timer, depth, score, visited_nodes, curr_path):
+        node_string = ""
+        my_path = copy.copy(curr_path)
+        best_path = curr_path
+        curr_visited_nodes = copy.copy(visited_nodes)
+        curr_visited_nodes.add(curr_node)
+        my_path.append([curr_node.my_name(), timer])
+        if timer > self.MAX_TIMER:
+            return score, my_path
+        tmp_unvisited = self.node_set.difference(curr_visited_nodes)
+        self.debug.print(3, f"finding best path from {curr_node.my_name()} on"
+                            f"minute {timer}, depth {depth}, and score {score}")
+        self.debug.increase_indent()
+        for next_node in curr_visited_nodes:
+            node_string = node_string + " " + next_node.my_name()
+        self.debug.print(4, f"nodes already visited are:  {node_string}")
+        path_string = ""
+        for node, time in best_path:
+            path_string = path_string + " " + str(time) + ":" + node
+        self.debug.print(4, f"best path so far is: {path_string}")
+        best_val = score
+        node_string = ""
+        for next_node in tmp_unvisited:
+            node_string = node_string + " " + next_node.my_name()
+        self.debug.print(4, f"nodes remaining to check are:  {node_string}")
+        for next_node in tmp_unvisited:
+            self.debug.print(3, f"checking node {next_node.my_name()}")
+            cost = self.shortest_paths[self.name_to_index[curr_node.my_name()]][self.name_to_index[next_node.my_name()]]
+            valve_gain = next_node.flow * (self.MAX_TIMER - timer - cost - 1)
+            self.debug.increase_indent()
+            self.debug.print(4, f"cost to go from {curr_node.my_name()} to {next_node.my_name()} is {cost}")
+            if (timer + cost) > self.MAX_TIMER - 1:
+                self.debug.print(4, f"cost is too high, passing")
+                self.debug.decrease_indent()
+                pass
+            else:
+                self.debug.print(4, f"flow is {next_node.flow} so gain would be {valve_gain}")
+                new_val, new_path = self.find_best_path(next_node, timer + cost + 1, depth + 1, score + valve_gain,
+                                                        curr_visited_nodes, my_path)
+                if new_val > best_val:
+                    best_val = new_val
+                    best_path = new_path
+                self.debug.decrease_indent()
+        self.debug.print(4, f"returning from recursion")
+        self.debug.decrease_indent()
+        return best_val, best_path
 
 
 class Puzzle:
@@ -167,7 +257,7 @@ class Puzzle:
             self.debug.increase_indent()
             for line in file:
                 self.debug.print(4, f"matching on line {line.rstrip()}")
-                z = re.match("Valve (\w+) has flow rate=(\d+); tunnel.*to valves? (.+)", line.rstrip())
+                z = re.match(r"Valve (\w+) has flow rate=(\d+); tunnel.*to valves? (.+)", line.rstrip())
                 self.debug.print(4, f"found valve {z.group(1)} with flow {z.group(2)} and tunnels {z.group(3)}")
                 self.tunnel_map.add_node(z.group(1), z.group(2), z.group(3).split(', '), self.debug)
             self.tunnel_map.print()
@@ -177,9 +267,11 @@ class Puzzle:
 
     def solve(self):
         self.parse()
+        self.tunnel_map.find_all_shortest_paths()
         self.print()
         if self.puzzle_part == "a":
-            # return self.find_path()
-            return 0
+            output_value = self.tunnel_map.find_path()
+            print("output value is", output_value)
+            return output_value
         else:
             return 0
--- day16/test_puzzle.py
+++ day16/test_puzzle.py
@@ -3,16 +3,17 @@ from puzzle import Puzzle
 
 
 class MyTestCase(unittest.TestCase):
+    # @unittest.skip("Skipping testing part A with demo data, not yet implemented")
     def test_demo_part_a(self):
-        demo_a = Puzzle("demo_data.txt", "a", 4)
+        demo_a = Puzzle("demo_data.txt", "a", 2)
         answer = demo_a.solve()
         self.assertEqual(1651, answer)
 
-    @unittest.skip("Skipping testing part A with real data, not yet implemented")
+    # @unittest.skip("Skipping testing part A with real data, not yet implemented")
     def test_part_a(self):
         part_a = Puzzle("test_data.txt", "a", 1)
         answer = part_a.solve()
-        self.assertEqual(-1, answer)
+        self.assertEqual(1986, answer)
 
     @unittest.skip("Skipping testing part B with demo data, not yet implemented")
     def test_demo_part_b(self):
